0906310526a8ff3d76241ab2817bc7e2bf4c7dba
Lenovo <88533953+SleepDaemon@users.noreply.github.com>
2023-06-17 14:34:56 +0300

    includes addBook feature

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// default library
 let myLibrary = [
-    {title: "The Hobbit", author: "J.R.R. Tolkien", pages: 295, read: false},
+    {title: "The rate Hobbit", author: "J.R.R. Tolkien", pages: 295, read: false},
     {title: "The Stars", author: "B.R Smith", pages: 170, read: true},
 ];
+
+
+// define variables
+const addBookBttn = document.getElementById('addBookBtn');
+const bookForm = document.getElementById('addBookForm');
+const addBookModal = document.getElementById('addBookModal');
+const overlay = document.getElementById('overlay');
 
 // constructor
 function book(title, author, pages, read) {
@@ -10,12 +18,12 @@ function book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 };
-  
 
 // function that creates a book card for each book in the array, each array object gets its own div
 function createBookCard() {
     let bookGrid = document.getElementById('bookSlot');
-    for (let i = 0; i < myLibrary.length; i++) {
+    for (let book of myLibrary) {
+        console.log(book.title)
         const bookCard = document.createElement('div');
         const title = document.createElement('p');
         const author = document.createElement('p');
@@ -24,9 +32,9 @@ function createBookCard() {
         const readBtn = document.createElement('button');
         const removeBtn = document.createElement('button');
 
-        title.textContent = `"${myLibrary[i].title}"`;
-        author.textContent = myLibrary[i].author;
-        pages.textContent = `${myLibrary[i].pages} pages`;
+        title.textContent = `"${book.title}"`;
+        author.textContent = book.author;
+        pages.textContent = `${book.pages} pages`;
         removeBtn.textContent = 'Remove';
 
         if (book.isRead) {
@@ -54,9 +62,65 @@ function createBookCard() {
     }
 }
 
+//add book modal
+const showForm = () => {
+    console.log('BTN') 
+    bookForm.reset()
+    addBookModal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+// close book modal
+const closeAddBookModal = () => {
+    addBookModal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+// updates the bookgrid
+const updateBooksGrid = () => {
+    resetBooksGrid()
+    createBookCard(book)    
+}
+
+// resets the bookGrid
+const resetBooksGrid = () => {
+    const bookGrid = document.getElementById('bookSlot')
+    bookGrid.innerHTML = ''
+}
+
+// saves array to local
+const saveLocal = () => {
+    localStorage.setItem('library', JSON.stringify(myLibrary))
+}
+
+// restores from last local save
+const restoreLocal = () => {
+    myLibrary = JSON.parse(localStorage.getItem('library'))
+}
+
+// addBook function
+const addBook = (e) => {
+    e.preventDefault()
+
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead= document.getElementById('isRead').checked
+    console.log(title)
+    myLibrary.push(new book(title, author, pages, isRead))
+    console.log(myLibrary)
+    saveLocal()
+    closeAddBookModal()
+
+    updateBooksGrid()
+}
+
+// Event fucntions
+addBookForm.onsubmit = addBook
+addBookBttn.onclick = showForm
+
+// first time load
 window.onload = () => {
-    // for of loop to iterate through array
-    for (let book of myLibrary) {
-        createBookCard(book);
-    }
+    localStorage.clear();
+    createBookCard();
 };
